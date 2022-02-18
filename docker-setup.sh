1b398cb1f73662d567e92c06254baa57d3dd487a
Matt McHenry <mmchenry@carnegielearning.com>
2022-02-18 16:21:45 -0500

    docker-setup.sh: use nix-shell to ensure proper versions of node and npm

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,4 +1,12 @@
-#!/usr/bin/env bash
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash --pure -p bash nodejs-12_x cacert
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/e74894146a42ba552ebafa19ab2d1df7ccbc1738.tar.gz
+
+# above channel commit hash is nixos-21.05 ~ 2021-11-09
+
+#to execute this script, you only need to install nix-shell:
+#sh <(curl -L https://nixos.org/nix/install)
+#(see https://nixos.org/nix/ for more)
 
 set -e;
 set -u;
