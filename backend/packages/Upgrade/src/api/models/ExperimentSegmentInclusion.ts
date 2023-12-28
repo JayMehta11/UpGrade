8dbe460ed178e3af43c562eaadee59b689902109
RidhamShah <ridham.shah@ahduni.edu.in>
2023-12-28 10:39:11 +0530

    Update dependencies and primary key definitions in models

--- backend/packages/Upgrade/src/api/models/ExperimentSegmentInclusion.ts
+++ backend/packages/Upgrade/src/api/models/ExperimentSegmentInclusion.ts
@@ -1,17 +1,21 @@
-import { Entity, JoinColumn, OneToOne } from 'typeorm';
+import { Entity, JoinColumn, OneToOne, PrimaryColumn } from 'typeorm';
 import { BaseModel } from './base/BaseModel';
 import { Experiment } from './Experiment';
 import { Segment } from './Segment';
 
 @Entity()
 export class ExperimentSegmentInclusion extends BaseModel {
-  @OneToOne(() => Segment, (segment) => segment.experimentSegmentInclusion, { onDelete: 'CASCADE', primary: true })
+  @PrimaryColumn()
+  public segmentId: string;
+
+  @OneToOne(() => Segment, (segment) => segment.experimentSegmentInclusion, { onDelete: 'CASCADE' })
   @JoinColumn()
   public segment: Segment;
 
+  @PrimaryColumn()
+  public experimentId: string;
   @OneToOne(() => Experiment, (experiment) => experiment.experimentSegmentInclusion, {
     onDelete: 'CASCADE',
-    primary: true,
   })
   @JoinColumn()
   public experiment: Experiment;
